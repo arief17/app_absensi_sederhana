f697073b2bb929c4e96b1bd8985b462d8e9bb260
arif fahrudin <arif.fahrudin17@gmail.com>
2022-08-04 13:43:33 +0700

    ada perubahan di file datasekolah.php

--- page/datasekolah.php
+++ page/datasekolah.php
@@ -1,1 +1,51 @@
-datasekolah.php
+
+<h5>Tabel Data Sekolah</h5>
+<table class="table">
+  <thead>
+    <tr>
+    	<th scope="col">Opsi</th>
+      <th scope="col">#</th>
+      <th scope="col">Nisn</th>
+      <th scope="col">Nama Siswa</th>
+      <th scope="col">Tempat Lahir</th>
+      <th scope="col">Tanggal Lahir</th>
+      <th scope="col">No Hp</th>
+      <th scope="col">Alamat</th>
+      <th scope="col">JK</th>
+    </tr>	
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+       <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+       <td>Mark</td>
+      <td>Otto</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+       <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+       <td>Mark</td>
+      <td>Otto</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+       <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+       <td>Mark</td>
+      <td>Otto</td>
+    </tr>
+  </tbody>
+</table>
