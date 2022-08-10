2fe2c96ec3e75b943cb6cde06d750bb8bcfaee77
arif fahrudin <arif.fahrudin17@gmail.com>
2022-08-10 15:01:51 +0700

    manambahkan tabel datasekolah

--- page/datasekolah.php
+++ page/datasekolah.php
@@ -1,51 +1,49 @@
-
-<h5>Tabel Data Sekolah</h5>
 <table class="table">
+  <h4 class="mb-3">#Data Sekolah</h4>
+  <a href="#" class="btn btn-primary btn-sm col-sm-3" data-bs-toggle="modal" data-bs-target="#datasekolah">Tambah Data</a>
   <thead>
-    <tr>
-    	<th scope="col">Opsi</th>
+    <tr class="text-center">
       <th scope="col">#</th>
-      <th scope="col">Nisn</th>
-      <th scope="col">Nama Siswa</th>
-      <th scope="col">Tempat Lahir</th>
-      <th scope="col">Tanggal Lahir</th>
-      <th scope="col">No Hp</th>
-      <th scope="col">Alamat</th>
-      <th scope="col">JK</th>
-    </tr>	
+      <th scope="col">Aksi</th>
+      <th scope="col">Nama Sekolah</th>
+      <th scope="col">Alamat Sekolah</th>
+      <th scope="col">No Telpon</th>
+      <th scope="col">Email Sekolah</th>
+    </tr>
   </thead>
   <tbody>
     <tr>
       <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-       <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-       <td>Mark</td>
-      <td>Otto</td>
+      <td class="text-center"><a href="#">Edit</a> | <a href="#">Hapus</a></td>
+      <td>SMKN 1 Kragilan</td>
+      <td>Jl. Raya Serang - Jakart Km. 13</td>
+      <td>(0254) 88827878</td>
+      <td>smkn1kragilan@gmail.com</td>
     </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-       <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-       <td>Mark</td>
-      <td>Otto</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-       <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-       <td>Mark</td>
-      <td>Otto</td>
-    </tr>
+
   </tbody>
 </table>
+
+
+<!-- Modal -->
+<div class="modal fade" id="datasekolah" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Form Input Data Sekolah</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+
+
+          
+
+
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
